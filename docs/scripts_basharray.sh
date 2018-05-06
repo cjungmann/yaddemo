ec60f8c76267b3c3aafb52f0d99e18a97d4de8b4
Chuck Jungmann <chuckj60@gmail.com>
2018-05-06 12:29:03 -0500

    Increased font size because underscores are invisible when font size is 8.

--- docs/scripts_basharray.sh
+++ docs/scripts_basharray.sh
@@ -15,7 +15,7 @@
 ## to copy the code fragments.
 
 echo "PS1='\\[\\033[01;36m\\]user\\[\\033[0m\\] $ '" > /tmp/doctips_profile
-xterm -fa mono -fs 8 -geometry 86x20 -e bash --rcfile /tmp/doctips_profile &
+xterm -fa mono -fs 9 -geometry 86x20 -e bash --rcfile /tmp/doctips_profile &
 gedit scripts_basharray.sh
 unlink /tmp/doctips_profile
 
